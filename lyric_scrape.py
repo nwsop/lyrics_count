8d9bb45388e6f8aa859545a6aecc19c752603a71
Nathaniel Sop <nathanielsop@gmail.com>
2020-08-14 14:17:46 -0400

    Initial Commit

---
+++ lyric_scrape.py
@@ -0,0 +1,12 @@
+import bs4, requests, re
+page_data = requests.get("https://genius.com/Billie-eilish-my-future-lyrics")
+soup = bs4.BeautifulSoup(page_data.text, 'html.parser')
+# Finds div tag with class lyric
+lyric_list = soup.find(class_='lyrics')
+
+#Pulls lyrics from P tags
+lyrics = lyric_list.find_all('p')
+lyric_list.text
+
+print(lyric_list.get_text())
+
