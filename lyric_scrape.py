1f0599ab31c594a6a9182d56d712eadfb4c2bcf1
Nathaniel Sop <nathanielsop@gmail.com>
2020-08-20 00:32:37 -0400

    Third Commit, Regex and lyric count added

--- lyric_scrape.py
+++ lyric_scrape.py
@@ -1,4 +1,4 @@
-import bs4, requests, re
+import bs4, requests, re, pprint
 page_data = requests.get("https://genius.com/Billie-eilish-my-future-lyrics")
 soup = bs4.BeautifulSoup(page_data.text, 'html.parser')
 # Finds div tag with class lyric
@@ -9,6 +9,25 @@ lyrics = lyric_list.find_all('p')
 lyric_list.text
 
 s = re.sub(r'\[(.*?)\]', '', lyric_list.get_text())
+s = s.replace("(","")
+s = s.replace(")","")
+s = s.replace(",","")
 
-print(s)
+s = s.lower()
+
+lyric_split = s.split()
+
+d = {}
+
+for split in lyric_split:
+    if split not in d:
+        d[split] = 1
+    else:
+        d[split] += 1
+
+d = sorted(d.items(), key=lambda x: x[1], reverse=True)
+
+pp = pprint.PrettyPrinter(indent=2)
+pp.pprint(d)
+
 
